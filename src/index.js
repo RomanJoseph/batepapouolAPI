3ec688a7afc2744c161e190b4cd81776ece14e73
Shizuke136 <romanjose3008@gmail.com>
2022-09-05 04:20:09 -0300

    .env configured

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 // conectando ao banco
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
